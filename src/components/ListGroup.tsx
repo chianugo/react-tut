7b95e629d2b5aef9dd5732af4dfd252498e5be74
cko003 <cko003@bucknell.edu>
2024-07-05 16:07:15 +0100

    replace ternary operator with logical and

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,10 @@
 const ListGroup = () => {
   const cities = ["Lagos", "Benin", "Port Harcourt", "Zaria"];
-  const message = cities.length === 0 ? <p>No items found</p> : null;
+
   return (
     <>
       <h1>List</h1>
-      {message}
+      {cities.length === 0 && <p>No items found</p>}
       <ul className="list-group">
         {cities.map((city) => (
           // key=city serves as a unique identifier for react
