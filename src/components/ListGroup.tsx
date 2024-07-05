5d36860ce990e0f0ef444f49591fbea57cb5658c
cko003 <cko003@bucknell.edu>
2024-07-05 15:54:57 +0100

    dynamically render list depending on list length

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,16 @@
 const ListGroup = () => {
   const cities = ["Lagos", "Benin", "Port Harcourt", "Zaria"];
-
+  const message = cities.length === 0 ? <p>No items found</p> : null;
   return (
     <>
       <h1>List</h1>
+      {message}
       <ul className="list-group">
         {cities.map((city) => (
-          <li className="list-group-item">{city}</li>
+          // key=city serves as a unique identifier for react
+          <li className="list-group-item" key={city}>
+            {city}
+          </li>
         ))}
       </ul>
     </>
