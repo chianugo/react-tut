4beaaba0aa4202acf6fe3784cb58e813ed43d24f
cko003 <cko003@bucknell.edu>
2024-07-06 20:15:40 +0100

    use event handler on list items

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,17 @@
+import { MouseEvent } from "react";
+
 const ListGroup = () => {
   const cities = ["Lagos", "Benin", "Port Harcourt", "Zaria"];
 
+  const HandleClick = (event: MouseEvent) => console.log(event);
   return (
     <>
       <h1>List</h1>
       {cities.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {cities.map((city) => (
+        {cities.map((city, index) => (
           // key=city serves as a unique identifier for react
-          <li className="list-group-item" key={city}>
+          <li className="list-group-item" key={index} onClick={HandleClick}>
             {city}
           </li>
         ))}
