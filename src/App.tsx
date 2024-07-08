c7530d6c3f401d104428ac5b3076d364c3ede248
cko003 <cko003@bucknell.edu>
2024-07-08 11:23:15 +0100

    trigger dismissible alert with button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      <Button onClick={() => console.log("clicked")}>Primary Button</Button>
+      {alertVisible && (
+        <Alert onClick={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(true)}>Primary Button</Button>
     </div>
   );
 }
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,10 +2,22 @@ import { ReactNode } from "react";
 
 interface AlertProps {
   children: ReactNode;
+  onClick: () => void;
 }
 
-const Alert = ({ children }: AlertProps) => {
-  return <div className="alert alert-primary">{children}</div>;
+const Alert = ({ children, onClick }: AlertProps) => {
+  return (
+    <div className="alert alert-primary alert-dismissible">
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+        onClick={onClick}
+      ></button>
+    </div>
+  );
 };
 
 export default Alert;
